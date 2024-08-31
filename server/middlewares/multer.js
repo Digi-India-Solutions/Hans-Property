af0a4cb3e04e11438f12f5e083e92e2b0a4c7bd6
Harsh Vardhan Mishra <138967484+HARSH-VARDHAN-MISHRA@users.noreply.github.com>
2024-08-31 12:45:05 +0530

    update multer

--- server/middlewares/multer.js
+++ server/middlewares/multer.js
@@ -1,9 +1,12 @@
 const multer = require('multer');
+const fs = require("fs")
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, './uploads/Images')
-    // const uploadPath = path.join(__dirname, 'uploads', 'Images');
-    // cb(null, uploadPath);
+    const dir = "./uploads/Images";
+    if (!fs.existsSync(dir)) {
+      fs.mkdirSync(dir, { recursive: true });
+    }
+    cb(null, dir);
   },
   filename: function (req, file, cb) {
     const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9)
