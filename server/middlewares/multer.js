c19c2e2d52676975bda74368755316099e5194ed
Harsh Vardhan Mishra <138967484+HARSH-VARDHAN-MISHRA@users.noreply.github.com>
2024-08-31 12:30:44 +0530

    update multer.js

--- server/middlewares/multer.js
+++ server/middlewares/multer.js
@@ -1,7 +1,9 @@
 const multer = require('multer');
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, './uploads/Images')
+    // cb(null, './uploads/Images')
+    const uploadPath = path.join(__dirname, 'uploads', 'Images');
+    cb(null, uploadPath);
   },
   filename: function (req, file, cb) {
     const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9)
